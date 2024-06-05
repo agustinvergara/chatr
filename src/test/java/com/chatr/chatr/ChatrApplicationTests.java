6a5f526a6eb04e23ea122d03b730bf33eeb3114e
agustinvergara <agustin.vergara.ruiz@gmail.com>
2024-06-05 15:57:07 -0500

    :tada: STARTING THE JAVA PROJECT

---
+++ src/test/java/com/chatr/chatr/ChatrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatr.chatr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
